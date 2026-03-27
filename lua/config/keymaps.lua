94bb11618d389348d7e0cf9ded3f500a2f8cd282
jelemiekun[laptop-linux] <jeremieeden1946@gmail.com>
2026-03-27 17:57:03 +0800

    fix: shortcut for ctrl+alt+arrow up/down added for resize height

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<C-A-Up>", "<cmd>resize +1<CR>")
+vim.keymap.set("n", "<C-A-Down>", "<cmd>resize -1<CR>")
