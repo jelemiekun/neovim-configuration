d61a8eaa5faff5e954f814577b4edec0380f3404
jelemikun-fedora <jeremieeden1946@gmail.com>
2026-09-26 21:10:23 +0800

    feat: magma keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,51 @@
 vim.keymap.set("n", "<C-A-Up>", "<cmd>resize +1<CR>")
 vim.keymap.set("n", "<C-A-Down>", "<cmd>resize -1<CR>")
 vim.keymap.set("n", "<leader>cn", "<cmd>CellularAutomaton make_it_rain<CR>", { desc = "Cellular Automaton" })
+
+-- Magma Keymaps
+vim.keymap.set("n", "<leader>mi", "<cmd>MagmaInit<CR>", { desc = "Initialize a runtime" })
+vim.keymap.set("n", "<leader>md", "<cmd>MagmaDeinit<CR>", { desc = "Deinitialize the current runtime" })
+vim.keymap.set("n", "<leader>ml", "<cmd>MagmaEvaluateLine<CR>", { desc = "Evaluate the current line" })
+vim.keymap.set("x", "<leader>mv", ":<C-u>MagmaEvaluateVisual<CR>", {
+  desc = "Evaluate selected text",
+})
+vim.keymap.set(
+  "n",
+  "<leader>mo",
+  "<cmd>MagmaEvaluateOperator<CR>",
+  { desc = "Evaluate the text given by some operator" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>ma",
+  "<cmd>MagmaEvaluateArgument<CR>",
+  { desc = "Evaluate the text following this command" }
+)
+vim.keymap.set("n", "<leader>mc", "<cmd>MagmaReevaluateCell<CR>", { desc = "Reevaluate the currently selected cell" })
+vim.keymap.set(
+  "n",
+  "<leader>mD",
+  "<cmd>MagmaDelete<CR>",
+  { desc = "Delete the currently selected cell (do nothing if empty)" }
+)
+vim.keymap.set("n", "<leader>mO", "<cmd>MagmaShowOutput<CR>", { desc = "Open the output window of active cell" })
+vim.keymap.set("n", "<leader>mI", "<cmd>MagmaInterrupt<CR>", { desc = "Interrupts the currently running cell" })
+vim.keymap.set("n", "<leader>mr", "<cmd>MagmaRestart<CR>", { desc = "Shuts down and restarts the current kernel." })
+vim.keymap.set(
+  "n",
+  "<leader>ms",
+  "<cmd>MagmaSave<CR>",
+  { desc = "Save the current cells and evaluated outputs into a JSON file" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>mL",
+  "<cmd>MagmaLoad<CR>",
+  { desc = "Load the cells and evaluated outputs stored in a given JSON file" }
+)
+vim.keymap.set(
+  "n",
+  "<leader>me",
+  "<cmd>MagmaEnterOutput<CR>",
+  { desc = "Enter the output window, if it is currently open." }
+)
